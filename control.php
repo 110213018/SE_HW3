4a8c5836f069f71d108c9ef9b2650fc7b541efab
KAANSENG\kaans <kaanseng@gmail.com>
2023-11-18 15:46:39 +0800

    109213044 簡嘉成 1.0

--- control.php
+++ control.php
@@ -1,1 +1,41 @@
-c ontrol
+<?php
+require('model.php');
+
+/* 
+需要有這幾個變數: goodName, goodContent, price, buyNum, totalPrice, goodID
+(buyer)買方html界面: 需要設計輸入等的資料在欄位
+(seller)賣方html界面: 需要設計以上的變數
+*/
+
+$act=$_REQUEST['act']; // 輸出買方填入的資料表格
+switch ($act) {
+case "listJob":
+  $jobs=getJobList();
+  echo json_encode($jobs);
+  return;  
+
+case "addJob":
+    // 添加資料或是update資料  如果goodID=-1 就執行add
+	// 改 的button 是setEditUI 之前老師的todoView.html 裏面有這個程式可以用一下在 最下面
+	// Load Add Form 的button 是setAddUI 之前老師的todoView.html 程式碼的 最上面和最下面
+	$jsonStr = $_POST['dat'];
+	$job = json_decode($jsonStr);
+	addJob($job->goodName, $job->goodContent, $job->price, $job->buyNum, $job->totalPrice, $job->goodID);
+	return;
+
+/* update 的備用方案
+case "updateJob":
+    $id=(int)$_REQUEST['goodID'];
+    updateJob($id);
+    return;
+*/
+
+case "delJob":
+	$id=(int)$_REQUEST['goodID']; //$_GET, $_REQUEST
+	delJob($id);
+	return;
+default:
+  
+}
+
+?>
--- model.php
+++ model.php
@@ -1,1 +1,49 @@
-model
+<?php
+require('dbconfig.php');
+
+function getJobList() {
+	global $db;
+	$sql = "select * from shop;";
+	$stmt = mysqli_prepare($db, $sql ); //precompile sql指令，建立statement 物件，以便執行SQL
+	mysqli_stmt_execute($stmt); //執行SQL
+	$result = mysqli_stmt_get_result($stmt); //取得查詢結果
+
+	$rows = array(); //要回傳的陣列
+	while($r = mysqli_fetch_assoc($result)) {
+		$rows[] = $r; //將此筆資料新增到陣列中
+	}
+	return $rows;
+}
+
+function addJob($goodName,$goodContent,$price,$buyNum,$totalPrice,$goodID) {
+	global $db;
+	if($goodID>0) {
+		$sql = "update todo set goodName=?, goodContent=?, price=?, buyNum=?, totalPrice=? where goodID=?"; //SQL中的 ? 代表未來要用變數綁定進去的地方
+		$stmt = mysqli_prepare($db, $sql); //prepare sql statement
+		mysqli_stmt_bind_param($stmt, "ssiii", $goodName, $goodContent, $price, $buyNum, $totalPrice); //bind parameters with variables, with types "sss":string, string ,string
+	} else {
+		$sql = "insert into todo (goodName, goodContent, price, buyNum, totalPrice) values (?, ?, ?, ?, ?)"; //SQL中的 ? 代表未來要用變數綁定進去的地方
+		$stmt = mysqli_prepare($db, $sql); //prepare sql statement
+		mysqli_stmt_bind_param($stmt, "ssiii", $goodName, $goodContent, $price, $buyNum, $totalPrice); //bind parameters with variables, with types "sss":string, string ,string
+	}
+	mysqli_stmt_execute($stmt);  //執行SQL
+	return True;
+}
+
+/* update 的備用方案
+function updateJob($goodID, $goodName, $goodContent, $price, $buyNum, $totalPrice) {
+	echo $goodID, $goodName, $goodContent, $price, $buyNum, $totalPrice;
+	return;
+}
+*/
+
+function delJob($goodID) {
+	global $db;
+
+	$sql = "delete from todo where goodID=?;"; //SQL中的 ? 代表未來要用變數綁定進去的地方
+	$stmt = mysqli_prepare($db, $sql); //prepare sql statement
+	mysqli_stmt_bind_param($stmt, "i", $goodID); //bind parameters with variables, with types "sss":string, string ,string
+	mysqli_stmt_execute($stmt);  //執行SQL
+	return True;
+}
+?>
