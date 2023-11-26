2b93a380ff7bf0f3893a24e69eebba1402b72da2
KAANSENG\kaans <kaanseng@gmail.com>
2023-11-26 14:36:03 +0800

    Update contorl & model 1,2

--- control.php
+++ control.php
@@ -14,13 +14,30 @@ case "listJob":
   echo json_encode($jobs);
   return;  
 
+case "listCart":
+    $Carts=getCartList();
+	echo json_encode($Carts);
+	return;  
+
 case "addJob":
     // 添加資料或是update資料  如果goodID=-1 就執行add
 	// 改 的button 是setEditUI 之前老師的todoView.html 裏面有這個程式可以用一下在 最下面
 	// Load Add Form 的button 是setAddUI 之前老師的todoView.html 程式碼的 最上面和最下面
 	$jsonStr = $_POST['dat'];
 	$job = json_decode($jsonStr);
-	addJob($job->goodsName, $job->goodsContent, $job->price, $job->buyNum, $job->totalPrice, $job->totalQuantity, $job->id, $job->Quantity);
+	// addJob($job->goodsName, $job->goodsContent, $job->price, $job->buyNum, $job->totalPrice, $job->totalQuantity, $job->id, $job->Quantity);
+	addJob($job->goodsName, $job->goodsContent, $job->price, 6, 666, $job->totalQuantity, 0);
+	return;
+
+case "addCart":
+	// 添加資料或是update資料  如果goodID=-1 就執行add
+	// 改 的button 是setEditUI 之前老師的todoView.html 裏面有這個程式可以用一下在 最下面
+	// Load Add Form 的button 是setAddUI 之前老師的todoView.html 程式碼的 最上面和最下面
+	$id = $_REQUEST['id'];
+	// $Cart = json_decode($jsonStr);
+	echo($id);
+	// addJob($job->goodsName, $job->goodsContent, $job->price, $job->buyNum, $job->totalPrice, $job->totalQuantity, $job->id, $job->Quantity);
+	addCart($id);
 	return;
 
 /* update 的備用方案
@@ -34,6 +51,21 @@ case "delJob":
 	$id=(int)$_REQUEST['goodID']; //$_GET, $_REQUEST
 	delJob($id);
 	return;
+
+case "delJob_Cart":
+	$goodsID=(int)$_REQUEST['id']; //$_GET, $_REQUEST
+	delJob_Cart($goodsID);
+	return;
+
+case "gettotal":
+	$id=(int)$_REQUEST['goodID'];
+	gettotal($id);
+	return;
+
+case "checkout":
+	// $id=(int)$_REQUEST['goodID'];
+	checkout();
+	return;	
 default:
   
 }
--- model.php
+++ model.php
@@ -15,10 +15,24 @@ function getJobList() {
 	return $rows;
 }
 
-function addJob($goodsName,$goodsContent,$price,$buyNum, $totalPrice, $totalQuantity, $goodsID, $Quantity) {
+function getCartList() {
+	global $db;
+	$sql = "select * from shop where buyNum>0;";
+	$stmt = mysqli_prepare($db, $sql ); //precompile sql指令，建立statement 物件，以便執行SQL
+	mysqli_stmt_execute($stmt); //執行SQL
+	$result = mysqli_stmt_get_result($stmt); //取得查詢結果
+
+	$rows = array(); //要回傳的陣列
+	while($r = mysqli_fetch_assoc($result)) {
+		$rows[] = $r; //將此筆資料新增到陣列中
+	}
+	return $rows;
+}
+
+function addJob($goodsName,$goodsContent,$price,$buyNum, $totalPrice, $totalQuantity, $goodsID) {
 	global $db;
 	if($goodsID>0) {
-		$sql = "update shop set goodsName=?, goodsContent=?, price=?, buyNum=?, totalPrice=?, totalQuantity=?, Quantity=? where id=?"; //SQL中的 ? 代表未來要用變數綁定進去的地方
+		$sql = "update shop set goodsName=?, goodsContent=?, price=?, buyNum=?, totalPrice=?, totalQuantity=? where id=?"; //SQL中的 ? 代表未來要用變數綁定進去的地方
 		$stmt = mysqli_prepare($db, $sql); //prepare sql statement
 		mysqli_stmt_bind_param($stmt, "ssiiiiii", $goodsName, $goodsContent, $price, $buyNum, $totalPrice, $totalQuantity, $Quantity, $goodsID);
 	} else {
@@ -46,10 +60,31 @@ function delJob($goodsID) {
 	mysqli_stmt_execute($stmt);  //執行SQL
 	return True;
 }
+function delJob_Cart($goodsID) {
+	global $db;
+    
+	$sql = "update shop set totalQuantity=totalQuantity+buyNum, buyNum=0, totalPrice=0 where id=?;"; //SQL中的 ? 代表未來要用變數綁定進去的地方
+	$stmt = mysqli_prepare($db, $sql); //prepare sql statement
+	mysqli_stmt_bind_param($stmt, "i", $goodsID); //bind parameters with variables, with types "sss":string, string ,string
+	mysqli_stmt_execute($stmt);  //執行SQL
+	
+	return True;
+}
+
+function addCart($id) {
+	global $db;
+
+	$sql = "update shop set buyNum=buyNum+1, totalQuantity=totalQuantity-1 where totalQuantity>0 and id=?;";
+	$stmt = mysqli_prepare($db, $sql); //prepare sql statement
+	mysqli_stmt_bind_param($stmt, "i", $id); //bind parameters with variables, with types "sss":string, string ,string
+	mysqli_stmt_execute($stmt);  //執行SQL
+	gettotal($id);
+	return True;
+}
 function gettotal($goodsID) {
 	global $db;
 
-	$sql = "select sum(price * buyNum) as total from shop where id = ?;"; //SQL中的 ? 代表未來要用變數綁定進去的地方
+	$sql = "update shop set totalPrice=buyNum*price where id=?;"; //SQL中的 ? 代表未來要用變數綁定進去的地方
 	$stmt = mysqli_prepare($db, $sql); //prepare sql statement
 	mysqli_stmt_bind_param($stmt, "i", $goodsID); //bind parameters with variables, with types "sss":string, string ,string
 	mysqli_stmt_execute($stmt);
@@ -64,8 +99,9 @@ function gettotal($goodsID) {
 function checkout() {
     global $db;
 
-    $sql = "select sum(price * buyNum) from shop";
-    $stmt = mysqli_prepare($db, $sql);
+    // $sql = "select sum(price * buyNum) from shop1";
+    $sql = "update shop set buyNum=0, totalPrice=0 where 1;";
+	$stmt = mysqli_prepare($db, $sql);
     mysqli_stmt_execute($stmt);
 
     $result = mysqli_stmt_get_result($stmt);
